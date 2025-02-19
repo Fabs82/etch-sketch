530d3c4cd18a34093235115089b6561985d1f19c
Fabs82 <148846635+Fabs82@users.noreply.github.com>
2025-02-19 09:18:14 +0100

    Change squares background color when hovering with mouse

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const sketchContainer = document.querySelector(".sketchContainer");
+const sketchContainer = document.querySelector("#sketchContainer");
+const numberOfSquares = 16;
 
 function createSquareDiv() {
     const square = document.createElement("div");
@@ -6,10 +7,20 @@ function createSquareDiv() {
     sketchContainer.appendChild(square);
 }
 
-function createMultilpeDivs(number) {
-    for (let index = 0; index < number ** 2; index++) {
-        createSquareDiv()
+function createMultilpeDivs(numberOfSquares) {
+    for (let index = 0; index < numberOfSquares ** 2; index++) {
+        createSquareDiv();
     };
 };
 
-createMultilpeDivs(16);
+createMultilpeDivs(numberOfSquares);
+
+
+
+const squareNodes = document.querySelectorAll(".squareDiv");
+squareNodes.forEach(element => {
+    element.classList.add("newBackground");
+    element.addEventListener("mouseover", () => {
+        element.style.backgroundColor = "aqua";
+    });
+});
